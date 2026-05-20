ea410c4738865faa88d745c105c7bb18e3741a4b
Maite Youyoutte <maite.youyoutte@hotmail.fr>
2026-05-20 02:29:34 +0200

    Setup authentication routes controllers and associations

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,4 +1,6 @@
 class Folder < ApplicationRecord
   has_many :folder_invoices, dependent: :destroy
+
   has_many :invoices, through: :folder_invoices
+  has_many :users, through: :folder_invoices
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
   devise :database_authenticatable,
+         :registerable,
          :recoverable,
          :rememberable,
          :validatable
 
   has_many :invoices, dependent: :destroy
+
   has_many :folder_invoices, dependent: :destroy
+  has_many :folders, through: :folder_invoices
 end
