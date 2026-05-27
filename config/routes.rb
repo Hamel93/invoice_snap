3e6c4a0f87c69265c842f69c521019c726cc8e0e
Gabriel Benoit Hamel <info.gestiongbh@gmail.com>
2026-05-26 20:12:36 -0400

    Fix landing page authentication flow

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  devise_scope :user do
+    get "/logout", to: "devise/sessions#destroy"
+  end
+
   get "/login", to: redirect("/users/sign_in")
   get "/sign_in", to: redirect("/users/sign_in")
   get "/signup", to: redirect("/users/sign_up")
