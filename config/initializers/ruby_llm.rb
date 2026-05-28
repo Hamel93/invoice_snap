b25ac6ad2e0da930dd0439acf30653aed964ee90
Gabriel Benoit Hamel <info.gestiongbh@gmail.com>
2026-05-28 19:17:36 -0400

    Restore OpenAI provider for AI assistant

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -1,7 +1,3 @@
 RubyLLM.configure do |config|
   config.openai_api_key = ENV["OPENAI_API_KEY"]
-
-  if ENV["OPENAI_API_BASE"].present?
-    config.openai_api_base = ENV["OPENAI_API_BASE"]
-  end
 end
